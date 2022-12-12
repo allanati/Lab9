16afa30deb8a84dd716c90906f2ddb5984a89345
allanati <allanatiaganova@gmail.com>
2022-12-12 23:19:21 +0200

    Put class into main

--- Lab9.cpp
+++ Lab9.cpp
@@ -1,11 +1,59 @@
-#include "Header.hpp"
+#define _USE_MATH_DEFINES
+#include <cmath>
 #include <iostream>
+
+class circle
+{
+	private:
+		double m_rad = 0;
+		long double  m_area = 0;
+
+	public:
+		circle(double rad)
+		{
+			m_rad = rad;
+			m_area = CircArea(rad);
+		}
+
+		~circle()
+		{
+			std::cout << "Destructor works!";
+		}
+
+		bool SetRad(double rad)
+		{
+			if (rad <= 0)
+			{
+				std::cout << "Error!" << std::endl;
+				return false;
+			}
+			m_rad = rad;
+		}
+
+		double CircArea(double rad)
+		{
+			if (SetRad(rad))
+			{
+				m_area = m_rad * m_rad * M_PI;
+				return m_area;
+			}
+			return m_area;
+
+		}
+		void PrintArea()
+		{
+			if (m_area == 0) 
+				std::cout << "The circle does not exist!" << std::endl; 
+			else 
+				std::cout << m_area << std::endl;
+		}
+};
 
 int main()
 {
-	float r;
-	std::cout << "Enter radius of the circle: ";
-	std::cin >> r;
-	an::Circle circle1(r);
-	std::cout << "Sqr surface: " << circle1.SquareSurface();
+	std::cout << "Enter radius:";
+	double x;
+	std::cin >> x;
+	circle circle1(x);
+	circle1.PrintArea();
 }
